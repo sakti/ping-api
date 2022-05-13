ad9122a2340cd36401995e4186cff99bb912c8dc
Sakti Dwi Cahyono <sakti@sampingan.co.id>
2022-05-13 18:07:18 +0700

    feat: update default route & response

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
 use axum::{routing::get, Router};
 use sync_wrapper::SyncWrapper;
 
+async fn home() -> &'static str {
+    "welcome"
+}
+
 async fn pong() -> &'static str {
-    "Hello, world!"
+    "ping"
 }
 
 #[shuttle_service::main]
 async fn axum() -> Result<SyncWrapper<Router>, shuttle_service::Error> {
-    let router = Router::new().route("/ping", get(pong));
+    let router = Router::new()
+        .route("/", get(home))
+        .route("/ping", get(pong));
     let sync_wrapper = SyncWrapper::new(router);
 
     Ok(sync_wrapper)
